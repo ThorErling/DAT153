3a6af3670ed35228e66f86831db363dbdecec5c4
SpaceGoat47 <henrik.tveit14@gmail.com>
2022-01-19 14:23:05 +0100

    testing

---
+++ app/src/main/java/com/example/dat153labweek3/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.dat153labweek3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView=findViewById(R.id.date);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        String currentDateandTime = sdf.format(new Date());
+        textView.setText(currentDateandTime);
+    }
+}
